d8876f7d14eaabb0d36480b987011329dcd80a53
Issei Hasegawa <149334273+IsseiHasegawa@users.noreply.github.com>
2026-05-20 20:56:45 -0400

    Write test file

---
+++ Test/src/hello.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    
+    return 0;
+}
+
+void hello(){
+    std::cout << "Hello world" << std::endl;
+}
+
+int TwoSum(int a, int b) {
+    int sum;
+    return sum = a + b;
+}
+
+double FTwoSum(double a, double b) {
+    double fsum;
+    return fsum = a + b;
+}
+
+std::string ReturnString(std::string s) {
+    return s;
+}
