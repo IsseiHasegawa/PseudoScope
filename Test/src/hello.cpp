b613a51c84b1cd1773a3e9231fa018a785a51ef4
Issei Hasegawa <149334273+IsseiHasegawa@users.noreply.github.com>
2026-05-20 21:23:14 -0400

    Add pseudoscope test

--- Test/src/hello.cpp
+++ Test/src/hello.cpp
@@ -1,23 +1,17 @@
+#include "hello.hpp"
+
 #include <iostream>
-#include <string>
 
-int main() {
-    
-    return 0;
-}
-
-void hello(){
+void hello() {
     std::cout << "Hello world" << std::endl;
 }
 
 int TwoSum(int a, int b) {
-    int sum;
-    return sum = a + b;
+    return a + b;
 }
 
 double FTwoSum(double a, double b) {
-    double fsum;
-    return fsum = a + b;
+    return a + b;
 }
 
 std::string ReturnString(std::string s) {
